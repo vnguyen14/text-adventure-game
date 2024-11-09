596ff9b870484fd28728aabd05a23cffc0f80a1b
grcng <viringuyen.work@gmail.com>
2024-11-08 20:10:37 -0500

    add comments about game logic

---
+++ Modules/rooms.py
@@ -0,0 +1,7 @@
+# def get_room_in_direction(self, direction):
+#          """Returns the room in the specified direction."""
+#          return self.directions.get(direction, None)
+
+# def get_long_description(self):
+
+# def get_short_description(self):
--- game_engine/home_logic.py
+++ game_engine/home_logic.py
@@ -1,3 +1,7 @@
 # Get long and short description from rooms class file
 
 print("this is home logic")
+
+# get_action_input(): get input from user to print out message
+# print message: "let's start a new day"
+# get_action_input():
